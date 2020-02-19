45f02a59ac7eca31a0b38c264d8bbc41a2ddd25d
Lasantha Basnayake <lasantha57@gmail.com>
2020-02-19 19:45:35 +0800

    Find all permutations

--- algorithms/permutation.js
+++ algorithms/permutation.js
@@ -19,5 +19,29 @@ function isPermutation(str1, str2) {
 
 }
 
-
 console.log(isPermutation('BASEBALL', 'BALLBASE'));
+
+
+/**
+ * Find all permutations
+ */
+
+var permArr = [];
+var usedChars = [];
+
+function permute(input) {
+    var i, ch;
+    for (i = 0; i < input.length; i++) {
+        ch = input.splice(i, 1)[0];
+        usedChars.push(ch);
+        if (input.length == 0) {
+            permArr.push(usedChars.slice());
+        }
+        permute(input);
+        input.splice(i, 0, ch);
+        usedChars.pop();
+    }
+    return permArr
+};
+
+console.log(permute([5, 3, 7, 1]));
