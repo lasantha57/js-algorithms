7ab89ff358f72192bad207e322b1043fda505c4d
Lasantha Basnayake <lasantha57@gmail.com>
2022-03-26 11:32:52 +0530

    update fibonacci logis

--- algorithms/fibonacci.js
+++ algorithms/fibonacci.js
@@ -57,7 +57,26 @@ function slowFib(n) {
 
 const fib = memoize(slowFib);
 
+// ----
 
+function fibonacci(n) {
+
+    const fib = [0, 1];
+
+    if (n <= 0) {
+        return [];
+    }
+
+    if (n === 1) {
+        return [0];
+    }
+
+    for (let i = 2; i <= n - 1; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    return fib;
+}
 
 /**
  * Interviewer: What is the run time complexity?
